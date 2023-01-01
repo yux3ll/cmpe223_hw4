a387a36f78c63faaf4d62e74aa6504f4f7b2494b
yux3ll <yuksel.baypinar@tedu.edu.tr>
2023-01-01 13:37:07 +0300

    implementation stuff, not complete

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,7 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Driver {
     //-----------------------------------------------------
     // Title: Driver
@@ -8,7 +12,15 @@ public class Driver {
     // Description:
     //-----------------------------------------------
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        System.out.println("Enter input filename:");
+        Scanner sc = new Scanner(System.in); // create a new Scanner object called sc
+        String filePath = sc.nextLine(); // read the file path from the console
+        Scanner input = new Scanner(new File(filePath)); // create a new Scanner object called input, and read the file from the file path
+        System.out.println("Enter the maximum average waiting time:");
+        final int MAX_WAIT_TIME = sc.nextInt(); // read the maximum average waiting time from the console
+
 
 
      }
